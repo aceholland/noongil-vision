b0780b77e9ee47c2923f9aa60d81428b1df26149
Your New Name <vaedraglobaldeveloper@gmail.com>
2026-09-22 23:04:23 +0530

    whatever

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'noongil-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './detect.html',
+  './manifest.json',
+  './favicon.svg',
+  './icon.svg',
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './city_background.png',
+  './background.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event (Network First, falling back to Cache)
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) return cachedResponse;
+          if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('./index.html');
+          }
+        });
+      })
+  );
+});
